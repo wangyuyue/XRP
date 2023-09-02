6fb0d6818687b7fb545bae116d722a057654d3e6
wangyuyue <wangyuyue99@gmail.com>
2023-09-02 17:12:24 +0000

    add csd test

--- test_bpfkv.sh
+++ test_bpfkv.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$(uname -r)" !=  "5.12.0-xrp+" ]; then
     printf "Not in XRP kernel. Please run the following commands to boot into XRP kernel:\n"
     printf "    sudo grub-reboot \"Advanced options for Ubuntu>Ubuntu, with Linux 5.12.0-xrp+\"\n"
@@ -36,8 +37,14 @@ sudo rm -rf $MOUNT_POINT/*
 printf "Creating a 5-layer database file...\n"
 sudo ./simplekv $DB_PATH 5 create
 
+printf "Running a short point lookup benchmark with regular file operation...\n"
+sudo ./simplekv $DB_PATH 5 get --requests=20000
+
 printf "Running a short point lookup benchmark with XRP enabled...\n"
-sudo ./simplekv $DB_PATH 5 get --requests=1000 --use-xrp
+sudo ./simplekv $DB_PATH 5 get --requests=20000 --use-xrp
+
+printf "Running a short point lookup benchmark with csd resubmit...\n"
+sudo ./simplekv $DB_PATH 5 get --requests=20000 --use-csd
 
 popd
 printf "Done.\n"
