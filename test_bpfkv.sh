5310bae25a0022408cbfe2a3657a54ade329b7bd
wangyuyue <wangyuyue99@gmail.com>
2023-09-02 21:26:24 +0000

    rawdisk kv

--- test_bpfkv.sh
+++ test_bpfkv.sh
@@ -10,14 +10,14 @@ SCRIPT_PATH=`realpath $0`
 BASE_DIR=`dirname $SCRIPT_PATH`
 BPFKV_PATH="$BASE_DIR/BPF-KV"
 UTILS_PATH="$BASE_DIR/utils"
-MOUNT_POINT="/mnt/xrp"
-DB_PATH="$MOUNT_POINT/bpfkv_test_db"
 
 DEV_NAME="/dev/nvme0n1"
 if [ ! -z $1 ]; then
     DEV_NAME=$1
 fi
 printf "DEV_NAME=$DEV_NAME\n"
+
+DB_PATH=$DEV_NAME
 
 # Check whether BPF-KV is built
 if [ ! -e "$BPFKV_PATH/simplekv" ]; then
@@ -28,12 +28,8 @@ fi
 # Disable CPU frequency scaling
 $UTILS_PATH/disable_cpu_freq_scaling.sh
 
-# Mount disk
-$UTILS_PATH/mount_disk.sh $DEV_NAME $MOUNT_POINT
-
 pushd $BPFKV_PATH
 
-sudo rm -rf $MOUNT_POINT/*
 printf "Creating a 5-layer database file...\n"
 sudo ./simplekv $DB_PATH 5 create
 
